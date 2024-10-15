886889e85a81921a3f9b908bb81dd0e6f49fc907
ciekotics <rahul.saxena@jabotics.in>
2024-10-15 16:14:20 +0530

    importers edit/delete

---
+++ src/components/table/table-toolbar-actions.tsx
@@ -0,0 +1,78 @@
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogClose,
+  DialogDescription,
+} from '@/components/ui/dialog'
+import { Separator } from '@/components/ui/separator'
+import { MdEdit, MdDeleteOutline } from 'react-icons/md'
+import { Button } from '../custom/button'
+
+const TableToolbarActions = ({
+  label,
+  open,
+  setOpen,
+  children,
+  handleDelete,
+}: {
+  label: 'Edit' | 'Delete'
+  open: boolean
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+  children?: React.ReactNode
+  handleDelete?: () => Promise<void>
+}) => {
+  return (
+    <Dialog open={open}>
+      <DialogTrigger asChild>
+        <Button
+          variant={label === 'Edit' ? 'new_secondary' : 'outline'}
+          onClick={() => setOpen(true)}
+        >
+          {label === 'Edit' ? (
+            <MdEdit size={15} />
+          ) : (
+            <MdDeleteOutline size={15} />
+          )}
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogTitle>
+          {label === 'Edit' ? 'Update Details' : 'Delete Details'}
+        </DialogTitle>
+        <Separator />
+        <DialogDescription className='sr-only'>Table toolbar actions component</DialogDescription>
+
+        {children ?? (
+          <span className='mb-5 flex flex-col gap-1'>
+            <span className='text-lg'>
+              Are you sure, you want to delete this record?
+            </span>
+            <span className='text-sm text-gray-500'>
+              If you are sure, Confirm else Cancel.
+            </span>
+          </span>
+        )}
+
+        <div className='flex items-center gap-2'>
+          <DialogClose
+            onClick={() => setOpen(false)}
+            className={`w-full text-sm text-gray-400`}
+          >
+            Cancel
+          </DialogClose>
+          <DialogClose
+            onClick={handleDelete}
+            className={`w-full bg-gray-800 text-sm text-white ${label === 'Edit' ? 'hidden' : 'inline-block py-2'}`}
+          >
+            Confirm
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default TableToolbarActions
--- src/pages/import-contacts/function.tsx
+++ src/pages/import-contacts/function.tsx
@@ -1,11 +1,64 @@
 import { Data } from './schema'
+import { useState } from 'react'
+import TableToolbarActions from '@/components/table/table-toolbar-actions'
+import FormComponent from './@modify-data/form-component'
+import { useRemoveImporterMutation } from '@/store/actions/slices/importersSlice'
 
 export const Address = ({ data }: { data: Data }) => {
-  return <div className='w-full h-full flex items-center justify-center'>
-    <span className='w-[400px] text-xs'>{data.address}</span>
-  </div>
+  return (
+    <div className='flex h-full w-full items-center justify-center'>
+      <span className='w-[400px] text-xs'>{data.address}</span>
+    </div>
+  )
 }
 
 export const GSTNumber = ({ data }: { data: Data }) => {
   return <>{data.gst_number}</>
+}
+
+export const ToolbarAction = ({ data }: { data: Data }) => {
+  const [Delete] = useRemoveImporterMutation()
+
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+
+  const [editOpen, setEditOpen] = useState(false)
+  const [deleteOpen, setDeleteOpen] = useState(false)
+
+  async function handleDelete() {
+    if (data && data._id) {
+      setIsSubmitting(true)
+      try {
+        const res = await Delete({ id: data._id }).unwrap()
+
+        if (res.status === 'fail') throw new Error(res.message)
+
+        setDeleteOpen(false)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsSubmitting(false)
+      }
+    }
+  }
+
+  return (
+    <div className='flex items-center justify-center gap-2'>
+      <TableToolbarActions open={editOpen} setOpen={setEditOpen} label='Edit'>
+        <FormComponent
+          data={data}
+          setOpen={setEditOpen}
+          toEdit={true}
+          isSubmitting={isSubmitting}
+          setIsSubmitting={setIsSubmitting}
+        />
+      </TableToolbarActions>
+
+      <TableToolbarActions
+        open={deleteOpen}
+        setOpen={setDeleteOpen}
+        label='Delete'
+        handleDelete={handleDelete}
+      />
+    </div>
+  )
 }
